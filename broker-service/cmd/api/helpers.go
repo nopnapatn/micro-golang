5a4bca88456534b28ca6c470eed6b73f3cff2bda
nopnapatn <nopnapatn@gmail.com>
2024-02-17 15:16:23 +0700

    feat(broker-service): add helpers to handler json

---
+++ broker-service/cmd/api/helpers.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1048576
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	e := dec.Decode(data)
+	if e != nil {
+		return e
+	}
+
+	e = dec.Decode(&struct{}{})
+	if e != io.EOF {
+		return errors.New("body must have only a single JSON value")
+	}
+
+	return nil
+}
+
+func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, e := json.Marshal(data)
+	if e != nil {
+		return e
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, e = w.Write(out)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func (app *Config) errorJSON(w http.ResponseWriter, e error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = e.Error()
+
+	return app.writeJSON(w, statusCode, payload)
+}
